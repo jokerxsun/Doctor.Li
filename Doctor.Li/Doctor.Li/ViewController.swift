ee89183651aebef33271ee30ee051167611461c2
孙旭 <sun1003103044@163.com>
2018-01-02 21:55:59 +0800

    Project init

---
+++ Doctor.Li/Doctor.Li/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Doctor.Li
+//
+//  Created by sunxu on 2018/1/2.
+//  Copyright © 2018年 JokerSun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
