5ab01bb8c8a594dc5eefa7e613e4136c1e4ddaca
孙旭 <sun1003103044@163.com>
2018-01-03 08:53:52 +0800

    ocr

--- Doctor.Li/Doctor.Li/AppDelegate.swift
+++ Doctor.Li/Doctor.Li/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow()
+        self.window?.backgroundColor = UIColor.white
+        self.window?.makeKeyAndVisible()
+    
         // Override point for customization after application launch.
         return true
     }
